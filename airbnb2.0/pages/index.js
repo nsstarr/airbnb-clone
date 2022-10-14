ae3ce580ade44e8d5a5bc87a671d818f97e9335a
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-10-14 16:29:50 +0100

    finish the banner

--- airbnb2.0/pages/index.js
+++ airbnb2.0/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      {/* Banner */}
+      <Banner/>
 
     </div>
   );
