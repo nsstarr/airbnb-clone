56b0bd1761af72640bda9a4908a0ca1af1b78c18
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-10-19 16:40:56 +0100

    set up dynamic routing between homepage and search page with useRouter and display date in the correct format using date-fns

--- airbnb2.0/components/Header.js
+++ airbnb2.0/components/Header.js
@@ -10,12 +10,15 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from 'next/dist/client/router'
 
-function Header() {
+
+function Header({placeholder}) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
@@ -28,11 +31,27 @@ function Header() {
     setEndDate(ranges.selection.endDate);
   };
 
+  const resetInput = () => {
+    setSearchInput("")
+  }
+
+  const search = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberOfGuests,
+      }
+    })
+  }
+
   console.log(searchInput);
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
       {/* left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div onClick ={() => router.push('/')} className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -46,7 +65,7 @@ function Header() {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
-          placeholder="Start your search"
+          placeholder={ placeholder || "Start your search"}
           className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
         />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-auto md:mx-2" />
@@ -83,8 +102,8 @@ function Header() {
             />
           </div>
           <div className="flex ">
-            <button className="flex-grow text-gray-500">Cancel</button>
-            <button className="flex-grow text-red-400">Search</button>
+            <button onClick = {resetInput} className="flex-grow text-gray-500">Cancel</button>
+            <button onClick = {search} className="flex-grow text-red-400">Search</button>
           </div>
         </div>
       )}
---
+++ airbnb2.0/pages/search.js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/dist/client/router";
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { format } from "date-fns";
+
+function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, numberOfGuests } = router.query;
+  
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+
+
+  return (
+    <div className="h-screen">
+      <Header placeholder = {`${location} | ${range} | ${numberOfGuests} guests`}/>
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} for {numberOfGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexbibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
