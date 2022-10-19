16b2344d04d0e7abde4cb7b3668d15304bf4925a
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-10-19 13:38:35 +0100

    style calendar interface, set up a number of guests, start & end date as a piece of state

--- airbnb2.0/components/Header.js
+++ airbnb2.0/components/Header.js
@@ -7,21 +7,27 @@ import {
   UsersIcon,
 } from "@heroicons/react/24/solid";
 import { useState } from "react";
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
-
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(new Date())
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
 
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
-    key:'Selection'
-  }
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   console.log(searchInput);
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
@@ -54,12 +60,34 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
-    {searchInput &&
-      <div>
-      <DateRangePicker
-        ranges={[selectionRange]}
-      />
-      </div>}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              {" "}
+              Number of Guests{" "}
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex ">
+            <button className="flex-grow text-gray-500">Cancel</button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
